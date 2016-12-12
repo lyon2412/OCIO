060d8b6c266d1f532c87302d6543e901492662c9
lyon2412 <lyon_24_12@hotmail.com>
2016-12-12 13:44:26 -0500

    Create ADRIANA 3.C

---
+++ ADRIANA 3.C
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void main(){
+
+	int i, n=0, m=0, d=0, c=0;
+
+	printf("ingrese un numero de 4 cifras: ");
+	scanf("%d", &n);
+
+	if(n-1000<0) 
+	m=0;
+	else{
+
+		for(i=0; i<10; i++){
+			if(n-(i*1000)<1000){
+				m=i;
+				n = n-m*1000;
+				break;
+			}
+		}
+	}
+	if(n-100<0) 
+	c=0;
+	else{
+		for(i=0; i<10; i++){
+			if(n-(i*100)<100){
+				c=i;
+				n = n-c*100;
+				break;
+			}
+		}
+	}
+	if(n-10<0) 
+	d=0;
+	else{
+		for(i=1; i<10; i++){
+			if(n-(i*10)<10){
+				d=i;
+				n = n-d*10;
+				break;
+			}
+		}
+	}
+printf("1: %d\n10: %d\n100: %d\n1000:%d\n", n, d, c, m );	
+				
+}
